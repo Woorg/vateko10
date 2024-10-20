23bea73cccdfbf861aa13c7350393de329b87138
Igor Gorlov <gorlov35@gmail.com>
2024-10-21 01:32:09 +0300

    commit wp redis

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,5 @@
 <?php
-define( 'WP_CACHE', false); // Added by WP Rocket
+define('WP_CACHE', false); // Added by WP Rocket
 
 /**
  * The base configuration for WordPress
@@ -27,27 +27,27 @@ $dotenv->load();
 
 // define('WP_MEMORY_LIMIT', '512M'); 
 
-define( 'WP_REDIS_HOST', $_ENV['WP_REDIS_HOST'] );
-define( 'WP_REDIS_PASSWORD', $_ENV['WP_REDIS_PASSWORD'] );
+// define( 'WP_REDIS_HOST', $_ENV['WP_REDIS_HOST'] );
+// define( 'WP_REDIS_PASSWORD', $_ENV['WP_REDIS_PASSWORD'] );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $_ENV['DB_NAME'] );
+define('DB_NAME', $_ENV['DB_NAME']);
 
 /** Database username */
-define( 'DB_USER', $_ENV['DB_USER'] );
+define('DB_USER', $_ENV['DB_USER']);
 
 /** Database password */
-define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 
 /** Database hostname */
-define( 'DB_HOST', $_ENV['DB_HOST'] );
+define('DB_HOST', $_ENV['DB_HOST']);
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define('DB_CHARSET', 'utf8mb4');
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication unique keys and salts.
@@ -60,14 +60,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'ZvsPMaPMs#4%02.lxfoIWNJ.qZ}OgCd~hb6.o+0v }tM<6k{;_<%O: Mp&&5{pbF' );
-define( 'SECURE_AUTH_KEY',  'o~#1BlBQ9G1=>^:Ls(JM nrsnr2C:,I<[9#u,JH(!.Po-zWW%|WEn4D+UycluL8i' );
-define( 'LOGGED_IN_KEY',    'uL3=}0jSXw^M+$9Ccb$Q}I8&Z_<ee!vj&KO1@}E]2W{UQcVyU0*!2<k[nHxrV$PP' );
-define( 'NONCE_KEY',        ';sNY|Ge2bQUUk|%^`j 9?2N9SA)!cFoF{4g%i8P-D;@DH*14]IdjIuu?N],O{HQ>' );
-define( 'AUTH_SALT',        'h;E$7!w$bpJ6b6 U&C1XUBsK<l-:W9mr^]ufMH/:R~dajknmKIJ*-$GFb{F8]8wr' );
-define( 'SECURE_AUTH_SALT', 'H9 PLADn#%:3O8O;;GV@%{Fqpz5z$.H[4Ck~8O>7=PJugYN13{nl>RCM1aq)ZY-D' );
-define( 'LOGGED_IN_SALT',   'n*z+Ua~ov$c7*`Ts*2!Q9ZAc&4,h_pE28Cn*zI1p<@-c&qQO[cEGYv}8|u5%j7{E' );
-define( 'NONCE_SALT',       '^gHkFa{>`YVm^/~VfyRTxr)$PR+n$;UXT<7$R[<WgF)@wL[(77-wM[{?qziFY$$>' );
+define('AUTH_KEY',         'ZvsPMaPMs#4%02.lxfoIWNJ.qZ}OgCd~hb6.o+0v }tM<6k{;_<%O: Mp&&5{pbF');
+define('SECURE_AUTH_KEY',  'o~#1BlBQ9G1=>^:Ls(JM nrsnr2C:,I<[9#u,JH(!.Po-zWW%|WEn4D+UycluL8i');
+define('LOGGED_IN_KEY',    'uL3=}0jSXw^M+$9Ccb$Q}I8&Z_<ee!vj&KO1@}E]2W{UQcVyU0*!2<k[nHxrV$PP');
+define('NONCE_KEY',        ';sNY|Ge2bQUUk|%^`j 9?2N9SA)!cFoF{4g%i8P-D;@DH*14]IdjIuu?N],O{HQ>');
+define('AUTH_SALT',        'h;E$7!w$bpJ6b6 U&C1XUBsK<l-:W9mr^]ufMH/:R~dajknmKIJ*-$GFb{F8]8wr');
+define('SECURE_AUTH_SALT', 'H9 PLADn#%:3O8O;;GV@%{Fqpz5z$.H[4Ck~8O>7=PJugYN13{nl>RCM1aq)ZY-D');
+define('LOGGED_IN_SALT',   'n*z+Ua~ov$c7*`Ts*2!Q9ZAc&4,h_pE28Cn*zI1p<@-c&qQO[cEGYv}8|u5%j7{E');
+define('NONCE_SALT',       '^gHkFa{>`YVm^/~VfyRTxr)$PR+n$;UXT<7$R[<WgF)@wL[(77-wM[{?qziFY$$>');
 
 /**#@-*/
 
@@ -93,8 +93,8 @@ $table_prefix = 'wp_';
  */
 define('WP_DEBUG', false);
 define('WP_DEBUG_LOG', false);
-define('WP_AUTO_UPDATE_CORE', true );
-define('WP_CACHE_KEY_SALT', $_ENV['WP_CACHE_KEY_SALT'] );
+define('WP_AUTO_UPDATE_CORE', true);
+define('WP_CACHE_KEY_SALT', $_ENV['WP_CACHE_KEY_SALT']);
 //define('DISALLOW_FILE_EDIT', true );
 //define('AUTOMATIC_UPDATER_DISABLED', false);
 //define( 'WP_HTTP_BLOCK_EXTERNAL', true );
@@ -110,8 +110,8 @@ define('WP_CACHE_KEY_SALT', $_ENV['WP_CACHE_KEY_SALT'] );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (! defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
