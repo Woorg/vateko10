7938abbf5db51d57703b2ca6bd5c53e06a52aa36
Igor Gorlov <gorlov35@gmail.com>
2024-10-21 01:25:11 +0300

    save as utf-8  tw-sack.js

--- wp-includes/js/tw-sack.js
+++ wp-includes/js/tw-sack.js
@@ -1,5 +1,5 @@
 /* Simple AJAX Code-Kit (SACK) v1.6.1 */
-/* ©2005 Gregory Wild-Smith */
+/* ï¿½2005 Gregory Wild-Smith */
 /* www.twilightuniverse.com */
 /* Software licenced under a modified X11 licence,
    see documentation or authors website for more details */
@@ -7,47 +7,47 @@
 function sack(file) {
 	this.xmlhttp = null;
 
-	this.resetData = function() {
-		this.method = "POST";
-  		this.queryStringSeparator = "?";
-		this.argumentSeparator = "&";
-		this.URLString = "";
+	this.resetData = function () {
+		this.method = 'POST';
+		this.queryStringSeparator = '?';
+		this.argumentSeparator = '&';
+		this.URLString = '';
 		this.encodeURIString = true;
-  		this.execute = false;
-  		this.element = null;
+		this.execute = false;
+		this.element = null;
 		this.elementObj = null;
 		this.requestFile = file;
 		this.vars = new Object();
 		this.responseStatus = new Array(2);
-  	};
-
-	this.resetFunctions = function() {
-  		this.onLoading = function() { };
-  		this.onLoaded = function() { };
-  		this.onInteractive = function() { };
-  		this.onCompletion = function() { };
-  		this.onError = function() { };
-		this.onFail = function() { };
 	};
 
-	this.reset = function() {
+	this.resetFunctions = function () {
+		this.onLoading = function () {};
+		this.onLoaded = function () {};
+		this.onInteractive = function () {};
+		this.onCompletion = function () {};
+		this.onError = function () {};
+		this.onFail = function () {};
+	};
+
+	this.reset = function () {
 		this.resetFunctions();
 		this.resetData();
 	};
 
-	this.createAJAX = function() {
+	this.createAJAX = function () {
 		try {
-			this.xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+			this.xmlhttp = new ActiveXObject('Msxml2.XMLHTTP');
 		} catch (e1) {
 			try {
-				this.xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				this.xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
 			} catch (e2) {
 				this.xmlhttp = null;
 			}
 		}
 
-		if (! this.xmlhttp) {
-			if (typeof XMLHttpRequest != "undefined") {
+		if (!this.xmlhttp) {
+			if (typeof XMLHttpRequest != 'undefined') {
 				this.xmlhttp = new XMLHttpRequest();
 			} else {
 				this.failed = true;
@@ -55,33 +55,36 @@ function sack(file) {
 		}
 	};
 
-	this.setVar = function(name, value){
+	this.setVar = function (name, value) {
 		this.vars[name] = Array(value, false);
 	};
 
-	this.encVar = function(name, value, returnvars) {
+	this.encVar = function (name, value, returnvars) {
 		if (true == returnvars) {
 			return Array(encodeURIComponent(name), encodeURIComponent(value));
 		} else {
-			this.vars[encodeURIComponent(name)] = Array(encodeURIComponent(value), true);
+			this.vars[encodeURIComponent(name)] = Array(
+				encodeURIComponent(value),
+				true
+			);
 		}
-	}
+	};
 
-	this.processURLString = function(string, encode) {
+	this.processURLString = function (string, encode) {
 		encoded = encodeURIComponent(this.argumentSeparator);
-		regexp = new RegExp(this.argumentSeparator + "|" + encoded);
+		regexp = new RegExp(this.argumentSeparator + '|' + encoded);
 		varArray = string.split(regexp);
-		for (i = 0; i < varArray.length; i++){
-			urlVars = varArray[i].split("=");
-			if (true == encode){
+		for (i = 0; i < varArray.length; i++) {
+			urlVars = varArray[i].split('=');
+			if (true == encode) {
 				this.encVar(urlVars[0], urlVars[1]);
 			} else {
 				this.setVar(urlVars[0], urlVars[1]);
 			}
 		}
-	}
+	};
 
-	this.createURLString = function(urlstring) {
+	this.createURLString = function (urlstring) {
 		if (this.encodeURIString && this.URLString.length) {
 			this.processURLString(this.URLString, true);
 		}
@@ -95,7 +98,7 @@ function sack(file) {
 		}
 
 		// prevents caching of URLString
-		this.setVar("rndval", new Date().getTime());
+		this.setVar('rndval', new Date().getTime());
 
 		urlstringtemp = new Array();
 		for (key in this.vars) {
@@ -106,20 +109,21 @@ function sack(file) {
 				key = encoded[0];
 			}
 
-			urlstringtemp[urlstringtemp.length] = key + "=" + this.vars[key][0];
+			urlstringtemp[urlstringtemp.length] = key + '=' + this.vars[key][0];
 		}
-		if (urlstring){
-			this.URLString += this.argumentSeparator + urlstringtemp.join(this.argumentSeparator);
+		if (urlstring) {
+			this.URLString +=
+				this.argumentSeparator + urlstringtemp.join(this.argumentSeparator);
 		} else {
 			this.URLString += urlstringtemp.join(this.argumentSeparator);
 		}
-	}
+	};
 
-	this.runResponse = function() {
+	this.runResponse = function () {
 		eval(this.response);
-	}
+	};
 
-	this.runAJAX = function(urlstring) {
+	this.runAJAX = function (urlstring) {
 		if (this.failed) {
 			this.onFail();
 		} else {
@@ -129,17 +133,21 @@ function sack(file) {
 			}
 			if (this.xmlhttp) {
 				var self = this;
-				if (this.method == "GET") {
-					totalurlstring = this.requestFile + this.queryStringSeparator + this.URLString;
+				if (this.method == 'GET') {
+					totalurlstring =
+						this.requestFile + this.queryStringSeparator + this.URLString;
 					this.xmlhttp.open(this.method, totalurlstring, true);
 				} else {
 					this.xmlhttp.open(this.method, this.requestFile, true);
 					try {
-						this.xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-					} catch (e) { }
+						this.xmlhttp.setRequestHeader(
+							'Content-Type',
+							'application/x-www-form-urlencoded'
+						);
+					} catch (e) {}
 				}
 
-				this.xmlhttp.onreadystatechange = function() {
+				this.xmlhttp.onreadystatechange = function () {
 					switch (self.xmlhttp.readyState) {
 						case 1:
 							self.onLoading();
@@ -163,22 +171,24 @@ function sack(file) {
 							if (self.elementObj) {
 								elemNodeName = self.elementObj.nodeName;
 								elemNodeName.toLowerCase();
-								if (elemNodeName == "input"
-								|| elemNodeName == "select"
-								|| elemNodeName == "option"
-								|| elemNodeName == "textarea") {
+								if (
+									elemNodeName == 'input' ||
+									elemNodeName == 'select' ||
+									elemNodeName == 'option' ||
+									elemNodeName == 'textarea'
+								) {
 									self.elementObj.value = self.response;
 								} else {
 									self.elementObj.innerHTML = self.response;
 								}
 							}
-							if (self.responseStatus[0] == "200") {
+							if (self.responseStatus[0] == '200') {
 								self.onCompletion();
 							} else {
 								self.onError();
 							}
 
-							self.URLString = "";
+							self.URLString = '';
 							break;
 					}
 				};
