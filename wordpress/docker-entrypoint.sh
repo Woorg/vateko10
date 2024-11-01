7432757cac74c492fc0ff7d16e446d3f9ee37ad1
Igor Gorlov <gorlov35@gmail.com>
2024-11-01 19:12:32 +0300

    debug dockerfile

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -6,6 +6,9 @@ set -e
 # Изменяем владельца папки /var/www/html
 chown -R www-data:www-data . /var/www/html
 
+# Изменяем права на файлы 
+chmod -R 755 /var/www/html
+
 # Запускаем php-fpm
 exec php-fpm
 
