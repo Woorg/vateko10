2bf76bba4cf6059469aa4aa1f20e63511788b74a
Igor Gorlov <gorlov35@gmail.com>
2024-11-01 16:56:39 +0300

    change entrypoint

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 # docker-entrypoint.sh
+
 set -e
 
-# Изменение прав доступа перед запуском php-fpm
-# COPY --chown=www-data:www-data wp-config.php /usr/src/wordpress/ # buildkit
-
-
-# Копируем и устанавливаем права на файлы WordPress
-# COPY . /var/www/html
-# RUN chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html
-
-
-# Копируем файлы в /var/www/html, если они еще не скопированы
-if [ ! -d "/var/www/html/wp-content" ]; then
-    cp -r ./* /var/www/html
-fi
-
-# Настройка прав на директорию
+# Изменяем владельца папки /var/www/html
 chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
 
-
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
-
+# Запускаем php-fpm
+exec php-fpm
 
 
 # Запуск PHP-FPM
