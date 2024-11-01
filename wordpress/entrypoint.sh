54b4bfca18957139f9454802bfa3c3314a0dd36a
Igor Gorlov <gorlov35@gmail.com>
2024-11-01 15:17:38 +0300

    change entrypoint

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -2,8 +2,10 @@
 # entrypoint.sh
 
 # Изменение прав доступа перед запуском php-fpm
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+chown -R www-data:www-data /var/www
+chmod -R 755 /var/www
+
+service redis-server start
 
 # Запуск PHP-FPM
 exec "$@"
