36cdaee6cb9cbebbe9a958bef221c766ddcc387b
Igor Gorlov <gorlov35@gmail.com>
2024-11-01 19:26:12 +0300

    debug entrypoint

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -5,10 +5,11 @@ set -e
 
 # Изменяем владельца папки /var/www/html
 chown -R www-data:www-data . /var/www/html
+chmod -R 755 /var/www/html
 
 # Изменяем права на файлы 
-find /var/www/html -type d -exec chmod 755 {} \;
-find /var/www/html -type f -exec chmod 644 {} \;
+# find /var/www/html -type d -exec chmod 755 {} \;
+# find /var/www/html -type f -exec chmod 644 {} \;
 
 
 # Запуск PHP-FPM
