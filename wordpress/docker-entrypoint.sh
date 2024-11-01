bbb72394ed9d041dfea803f6254ece6a67a050ee
Igor Gorlov <gorlov35@gmail.com>
2024-11-01 19:16:18 +0300

    debug entrypoint

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -1,17 +1,34 @@
+# #!/bin/bash
+# # docker-entrypoint.sh
+
+# set -e
+
+# # Изменяем владельца папки /var/www/html
+# chown -R www-data:www-data . /var/www/html
+
+# # Изменяем права на файлы 
+# chmod -R 755 /var/www/html
+
+# # Запускаем php-fpm
+# exec php-fpm
+
+
+# # Запуск PHP-FPM
+# exec "$@"
+
+
+
 #!/bin/bash
 # docker-entrypoint.sh
 
 set -e
 
 # Изменяем владельца папки /var/www/html
-chown -R www-data:www-data . /var/www/html
+chown -R www-data:www-data /var/www/html
 
-# Изменяем права на файлы 
-chmod -R 755 /var/www/html
+# Устанавливаем права доступа: 755 для папок и 644 для файлов
+find /var/www/html -type d -exec chmod 755 {} \;
+find /var/www/html -type f -exec chmod 644 {} \;
 
 # Запускаем php-fpm
 exec php-fpm
-
-
-# Запуск PHP-FPM
-exec "$@"
